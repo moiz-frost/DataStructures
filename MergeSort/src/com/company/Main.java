060131303eda690ac70a4da9ea89fd4944d10ff1
Abdul Moiz <desipher.ams@gmail.com>
2018-12-05 20:12:37 +0500

    Merge sort done

--- MergeSort/src/com/company/Main.java
+++ MergeSort/src/com/company/Main.java
@@ -14,17 +14,16 @@ public class Main {
             int mid = (left + right) / 2;
             mergeSort(arr, left, mid);
             mergeSort(arr,mid + 1, right);
-            merge(arr, left, mid + 1, right);
+            merge(arr, left, mid, right);
         }
     }
 
     private static void merge(int[] arr, int left, int mid, int right) {
         int [] temp = new int[right - left + 1];
-        int tempCounter = 0, i = left, j = mid;
-        while (temp[temp.length - 1] == 0) {
-            System.out.println(temp[temp.length - 1]);
-            System.out.println();
-            if (arr[i] <= arr[j]) {
+        int tempCounter = 0, i = left, j = mid + 1;
+        while (i <= mid && j <= right) {
+
+            if (arr[i] < arr[j]) {
                 temp[tempCounter] = arr[i];
                 i++;
             } else {
@@ -33,14 +32,20 @@ public class Main {
             }
             tempCounter++;
         }
-//        printArray(temp, 0, temp.length - 1);
-        tempCounter = 0;
-        for (int k = left; k <= right; k++) {
-            arr[k] = temp[tempCounter++];
+        while (i <= mid) {
+            temp[tempCounter] = arr[i];
+            i++;
+            tempCounter++;
         }
-//        printArray(arr, left, right);
-//        printArray(arr, right, arr.length - 1);
-//        System.out.println();
+        while (j <= right) {
+            temp[tempCounter] = arr[j];
+            j++;
+            tempCounter++;
+        }
+        for (int k = left, l = 0; k <= right; k++, l++) {
+            arr[k] = temp[l];
+        }
+
     }
 
     public static void printArray (int[] arr, int left, int right) {
